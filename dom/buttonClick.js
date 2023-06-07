3e87f6505017de91168f7ee12afb5673391353a3
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2023-06-07 13:19:21 +0530

    toggle added

--- dom/buttonClick.js
+++ dom/buttonClick.js
@@ -1,7 +1,18 @@
+var toggler = true;
 function ChangeHandler(){
-
-    let content = document.getElementById("contentChange")
+    
+    if(toggler){
+        toggler = !true;
+        // console.log(toggler);
+    var content = document.getElementById("contentChange")
     content.innerHTML = "Hey... you changed Me!";
     content.style.color = "red";
     content.style.fontFamily = "Helvetica";
+    }else{
+        var content = document.getElementById("contentChange")
+        content.innerHTML = "<h2>Hello World</h2>";
+        content.style.color = "black";
+        toggler = true;
+    }
+    
 }
