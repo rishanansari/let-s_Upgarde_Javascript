5c1526e3df506c7f9c034b99174edb5827606a9f
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2023-07-02 22:13:54 +0530

    dom updated

--- dom/buttonClick.js
+++ dom/buttonClick.js
@@ -5,11 +5,13 @@ function ChangeHandler(){
         toggler = !true;
         // console.log(toggler);
     var content = document.getElementById("contentChange")
+    document.getElementById('btn').innerText = "Back 🔙";
     content.innerHTML = "Hey... you changed Me!";
     content.style.color = "red";
     content.style.fontFamily = "Helvetica";
     }else{
         var content = document.getElementById("contentChange")
+        document.getElementById('btn').innerText = "Click Me";
         content.innerHTML = "<h2>Hello World</h2>";
         content.style.color = "black";
         toggler = true;
