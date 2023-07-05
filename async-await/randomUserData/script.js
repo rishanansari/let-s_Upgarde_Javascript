a2fe535e2aa3149ab4a8ab2824de1144dad3c2ac
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2023-07-06 00:49:12 +0530

    all done

--- async-await/randomUserData/script.js
+++ async-await/randomUserData/script.js
@@ -1,13 +1,190 @@
 //define all the vaiable with respect to thier id and classes.
 
-let imgCard = document.getElementById("img_card");
+// let imgCard = document.getElementById("img_card");
 let img  = document.getElementById("img");
 let subTitle = document.getElementById("sub_title");
 let content = document.getElementById("title_content");
 let getUsetBtn = document.getElementById("getUser");
 
+//different button to show the different content 
+let personBtn = document.getElementById("person");
+let mailBtn  = document.getElementById("mail");
+let calendarBtn = document.getElementById("calendar");
+let mapBtn = document.getElementById("map");
+let phoneBtn = document.getElementById("phone");
+let lockBtn = document.getElementById("lock");
+
 let url = "https://randomuser.me/api/";
 
+
+//here we added the on window load event that will load all the functionality 
+window.addEventListener("DOMContentLoaded",async()=>{
+    try{
+    let res = await fetch(url);
+    let jsonObj = await res.json();
+    let results = jsonObj.results;
+
+    results.map((data)=>{
+        console.log(data);
+        img.src = `${data.picture.large}`;
+        subTitle.innerHTML = `<h4>Hi, My name is</h4>`;
+        content.innerHTML = `<h1>${data.name.first} ${data.name.last}</h1>`;
+
+        personBtn.addEventListener('mouseover',async function(e) {
+            try{
+                subTitle.innerHTML = `<h4>Hi, My name is</h4>`;
+                content.innerHTML = `<h1>${data.name.first} ${data.name.last}</h1>`;
+
+            }catch(err){
+                console.log(err);
+            }
+            
+        })
+        mailBtn.addEventListener('mouseover',async function(e) {
+            try{
+                subTitle.innerHTML = `<h4>My email address is</h4>`;
+                content.innerHTML = `<h1>${data.email}</h1>`;
+
+            }catch(err){
+                console.log(err);
+            }
+            
+        })
+        calendarBtn.addEventListener('mouseover',async function(e) {
+            try{
+                let date = new Date(data.dob.date)
+                let dob = date.toLocaleDateString();
+                subTitle.innerHTML = `<h4>My birthday is</h4>`;
+                content.innerHTML = `<h1>${dob}</h1>`;
+
+            }catch(err){
+                console.log(err);
+            }
+            
+        })
+        mapBtn.addEventListener('mouseover',async function(e) {
+            try{
+                subTitle.innerHTML = `<h4>My address is</h4>`;
+                content.innerHTML = `<h1>${data.location.street.number} ${data.location.street.name}</h1>`;
+
+            }catch(err){
+                console.log(err);
+            }
+            
+        })
+        phoneBtn.addEventListener('mouseover',async function(e) {
+            try{
+                subTitle.innerHTML = `<h4>My phone number is</h4>`;
+                content.innerHTML = `<h1>${data.phone}</h1>`;
+
+            }catch(err){
+                console.log(err);
+            }
+            
+        })
+        lockBtn.addEventListener('mouseover',async function(e) {
+            try{
+                subTitle.innerHTML = `<h4>My password is</h4>`;
+                content.innerHTML = `<h1>${data.login.password}</h1>`;
+
+            }catch(err){
+                console.log(err);
+            }
+            
+        })
+    
+    })
+    }catch(err){
+       console.log(err); 
+    }
+})
+
+//here onclick on the get user button all functionaloity 
+
 getUsetBtn.addEventListener("click",async function(){
-    imgCard.innerHTML = `<h3>Loding...</h3>`;
+    
+    try{
+        let res = await fetch(url);
+        let jsonObj = await res.json();
+        let results = jsonObj.results;
+        console.log(results);
+
+        results.map((data)=>{
+            console.log(data);
+            img.src = `${data.picture.large}`;
+            subTitle.innerHTML = `<h4>Hi, My name is</h4>`;
+            content.innerHTML = `<h1>${data.name.first} ${data.name.last}</h1>`;
+
+            personBtn.addEventListener('mouseover',async function(e) {
+                try{
+                    subTitle.innerHTML = `<h4>Hi, My name is</h4>`;
+                    content.innerHTML = `<h1>${data.name.first} ${data.name.last}</h1>`;
+
+                }catch(err){
+                    console.log(err);
+                }
+                
+            })
+            mailBtn.addEventListener('mouseover',async function(e) {
+                try{
+                    subTitle.innerHTML = `<h4>My email address is</h4>`;
+                    content.innerHTML = `<h1>${data.email}</h1>`;
+
+                }catch(err){
+                    console.log(err);
+                }
+                
+            })
+            calendarBtn.addEventListener('mouseover',async function(e) {
+                try{
+                    let date = new Date(data.dob.date)
+                    let dob = date.toLocaleDateString();
+                    subTitle.innerHTML = `<h4>My birthday is</h4>`;
+                    content.innerHTML = `<h1>${dob}</h1>`;
+
+                }catch(err){
+                    console.log(err);
+                }
+                
+            })
+            mapBtn.addEventListener('mouseover',async function(e) {
+                try{
+                    subTitle.innerHTML = `<h4>My address is</h4>`;
+                    content.innerHTML = `<h1>${data.location.street.number} ${data.location.street.name}</h1>`;
+
+                }catch(err){
+                    console.log(err);
+                }
+                
+            })
+            phoneBtn.addEventListener('mouseover',async function(e) {
+                try{
+                    subTitle.innerHTML = `<h4>My phone number is</h4>`;
+                    content.innerHTML = `<h1>${data.phone}</h1>`;
+
+                }catch(err){
+                    console.log(err);
+                }
+                
+            })
+            lockBtn.addEventListener('mouseover',async function(e) {
+                try{
+                    subTitle.innerHTML = `<h4>My password is</h4>`;
+                    content.innerHTML = `<h1>${data.login.password}</h1>`;
+
+                }catch(err){
+                    console.log(err);
+                }
+                
+            })
+            
+        });
+
+        
+
+    }catch(err){
+        console.log(err);
+    }
 });
+
+
