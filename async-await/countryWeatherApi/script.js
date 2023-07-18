8d634890b311f16aed5ddbecb987ed29e2ec5751
ABID ANSARI <89006248+rishanansari@users.noreply.github.com>
2023-07-18 13:38:28 +0530

    weather app with countries

---
+++ async-await/countryWeatherApi/script.js
@@ -0,0 +1,8 @@
+document.body.innerHTML = `
+<div class="container>
+<div class="heading-container">
+<h1 class="heading">List of Countires with thier Weather Details</h1>
+</div>
+<div class="main-container id="mainContainer"></div>
+</div>
+`;
